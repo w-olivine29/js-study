4b2c8416d4937d469003e413375b5a8f76316654
w-olivine29 <w.olivine29@gmail.com>
2025-06-23 15:21:58 +0900

    Feat: [section01basic] 변수와 상수

---
+++ section01basic/src/step1varibale/variable.js
@@ -0,0 +1,13 @@
+console.log("start vsCode");
+
+let name = "자바스크립트"; // 변수 값 할당, 초기화
+
+name = "js"; // 변수 값 재할당
+console.log(name);
+
+// 카멜 케이스
+let isTrueOrFalse;
+
+// 상수 (값이 변경되는 것을 방지)
+const CONST_NAME = "상수";
+//CONST_NAME = "const"; //Uncaught TypeError: Assignment to constant variable.
